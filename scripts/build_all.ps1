21d30e6ee7e09890f1eb0a605afcd3b7d0a390ca
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-01 15:43:01 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,10 +1,9 @@
-& "$PSScriptRoot\..\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+& "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFilesPath "$PSScriptRoot\..\game\<mod folder>" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -OutputFilePrefix "OC_1.35" `
+  -ModName "Obsidian Conflict 1.35" `
+  -ModFilesPath "$PSScriptRoot\..\game\obsidian" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/poc-hl2-legacy-registry/releases/download/obsidian/obsidian_conflict_135_+_client_hotfix_without_mountfix.zip" `
+  -ModBaseFilesUrlHash "d72c0b3aa1eb218b2aae04c21f4843fcef402a4cfea2f9880f07772ab79b8edf" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
